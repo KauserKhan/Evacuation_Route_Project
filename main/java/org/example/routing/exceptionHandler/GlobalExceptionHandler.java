021551ffa5fe96f04c9bf2fbffbfbd2459499e80
Kauserkhan <kauserkhan07@gmail.com>
2025-12-12 20:43:00 +0100

    Added initial project code

--- main/java/org/example/routing/exceptionHandler/GlobalExceptionHandler.java
+++ main/java/org/example/routing/exceptionHandler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
-
 import java.time.Instant;
 import java.util.Map;
 
